58729d47e028ff8003387632be5a4e8bd36b547b
lvshclt <lvshclt@gmail.com>
2021-10-04 02:37:30 +0900

    A TO Z

--- js/background.js
+++ js/background.js
@@ -2,4 +2,4 @@ const $html = document.querySelector('html');
 const images = ['0.jpg', '1.jpg', '2.jpg'];
 
 const chosenImg = images[Math.floor(Math.random() * images.length)];
-$html.style.background = `url(/img/${chosenImg}) no-repeat center center fixed`;
+$html.style.background = `url(./img/${chosenImg}) no-repeat center center fixed`;
